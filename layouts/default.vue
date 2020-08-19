3f1c76af92f95b46902db680be04277b76757796
y-chan <y_chan_crypto@yahoo.co.jp>
2020-08-20 01:54:13 +0900

    delete not need section

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,38 +6,10 @@
       </v-container>
     </v-main>
     <v-footer
-      :absolute="!fixed"
+      :absolute="true"
       app
     >
       <span>&copy; {{ new Date().getFullYear() }} 兵庫県 新型コロナウイルスまとめサイトプロジェクト</span>
     </v-footer>
   </v-app>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
-</script>
